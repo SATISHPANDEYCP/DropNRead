17c6cbdc08c603c75a8d873b07fb78e4a9f64d3e
SATISH PANDEY <154319850+SATISHPANDEYCP@users.noreply.github.com>
2025-01-17 00:55:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(session({
   name: 'session',
   keys: [process.env.SESSION_KEY],
   maxAge: 24 * 60 * 60 * 1000,
-  //httpOnly: true,
+  httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
 }));
 
